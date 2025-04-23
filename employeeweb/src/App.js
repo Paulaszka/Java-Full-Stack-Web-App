90bcae3367f92d960b2ff77045b117ad479ebe28
Paulaszka <pkropkasz@gmail.com>
2025-04-23 15:29:13 +0200

    dashboard and no match site added

--- employeeweb/src/App.js
+++ employeeweb/src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from "./pages/header/Header"
+import Dashboard from './pages/dashboard/Dashboard';
+import NoMatch from './pages/noMatch/noMatch';
 
 function App() {
   return (
     <>
       <Header/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}></Route>
+        <Route path='*' element={<NoMatch/>}></Route>
+      </Routes>
     </>
   );
 }
